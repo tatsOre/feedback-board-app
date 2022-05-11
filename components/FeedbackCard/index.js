48ccd5fd6fc84aafa0bc28683e409730c318c30c
tatsOre <tatianaorezap@gmail.com>
2022-05-11 11:14:05 -0400

    Remove unnecessary files

--- components/FeedbackCard/index.js
+++
@@ -1,9 +0,0 @@
-import FeedbackCardDetails from './feedback-details'
-
-export default function FeedbackCard({ feedback }) {
-  return (
-    <div className="feedback-detail group bg-white text-[13px] rounded-10 cursor-pointer p-6 md:px-8">
-      <FeedbackCardDetails feedback={feedback} />
-    </div>
-  )
-}
